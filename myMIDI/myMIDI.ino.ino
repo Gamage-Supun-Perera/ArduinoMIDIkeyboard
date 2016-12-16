68ecbf9fae74e11c46153e9afddd6d1d8e6f03d6
G Supun Perera <supun.perera50@gmail.com>
2016-12-17 00:58:12 +0530

    Add files via upload

---
+++ myMIDI/myMIDI.ino.ino
@@ -0,0 +1,147 @@
+// Pin Definitions
+// Rows are connected to
+const int row1 = 2;
+const int row2 = 3;
+const int row3 = 4;
+const int row4 = 5;
+const int row5 = 6;
+
+// The 74HC595 uses a serial communication 
+// link which has three pins
+const int clock = 10;
+const int latch = 9;
+const int data = 8;
+
+
+uint8_t keyToMidiMap[37];
+
+boolean keyPressed[37];
+
+int noteVelocity = 127;
+
+
+// use prepared bit vectors instead of shifting bit left everytime
+int bits[] = { 
+  B11111110,
+  B11111101,
+  B11111011,
+  B11110111,
+  B11101111,
+  B11011111,
+  B10111111,
+  B01111111
+};
+
+// 74HC595 shift to next column
+void scanColumn(int value) {
+  digitalWrite(latch, LOW); //Pulls the chips latch low
+  shiftOut(data, clock, MSBFIRST, value); //Shifts out the 8 bits to the shift register
+  digitalWrite(latch, HIGH); //Pulls the latch high displaying the data
+}
+
+void setup() {
+  
+  // Map scan matrix buttons/keys to actual Midi note number. Lowest num 41 corresponds to F MIDI note.
+    int note=48;
+    for(int i=0;i<37;i++){
+      keyToMidiMap[i] = note;
+      note++;
+    }
+
+  // setup pins output/input mode
+  pinMode(data, OUTPUT);
+  pinMode(clock, OUTPUT);
+  pinMode(latch, OUTPUT);
+
+  pinMode(row1, INPUT);
+  pinMode(row2, INPUT);
+  pinMode(row3, INPUT);
+  pinMode(row4, INPUT);
+
+    Serial.begin(31250);
+
+  delay(1000);
+
+}
+
+void loop() {
+
+  for (int col = 0; col < 8; col++) {
+    
+    // shift scan matrix to following column
+    scanColumn(bits[col]);
+
+    // check if any keys were pressed - rows will have HIGH output in this case corresponding
+    int groupValue1 = !digitalRead(row1);
+    int groupValue2 = !digitalRead(row2);
+    int groupValue3 = !digitalRead(row3);
+    int groupValue4 = !digitalRead(row4);
+    int groupValue5 = !digitalRead(row5);
+
+    // process if any combination of keys pressed
+     // process if any combination of keys pressed
+    if (groupValue1 != 0 || groupValue2 != 0 || groupValue3 != 0
+        || groupValue4 != 0 || groupValue5 != 0) {
+
+      if (groupValue1 != 0 && !keyPressed[col]) {
+        keyPressed[col] = true;
+        noteOn(0x91, keyToMidiMap[col], noteVelocity);
+      }
+
+      if (groupValue2 != 0 && !keyPressed[col + 8]) {
+        keyPressed[col + 8] = true;
+        noteOn(0x91, keyToMidiMap[col + 8], noteVelocity);
+      }
+
+      if (groupValue3 != 0 && !keyPressed[col + 16]) {
+        keyPressed[col + 16] = true;
+        noteOn(0x91, keyToMidiMap[col + 16], noteVelocity);
+      }
+
+      if (groupValue4 != 0 && !keyPressed[col + 24]) {
+        keyPressed[col + 24] = true;
+        noteOn(0x91, keyToMidiMap[col + 24], noteVelocity);
+      }
+      if (groupValue5 != 0 && !keyPressed[col + 32]) {
+        keyPressed[col + 32] = true;
+        noteOn(0x91, keyToMidiMap[col + 32], noteVelocity);
+      }
+
+
+    }
+
+    //  process if any combination of keys released
+    if (groupValue1 == 0 && keyPressed[col]) {
+      keyPressed[col] = false;
+      noteOn(0x91, keyToMidiMap[col], 0);
+    }
+
+    if (groupValue2 == 0 && keyPressed[col + 8]) {
+      keyPressed[col + 8] = false;
+      noteOn(0x91, keyToMidiMap[col + 8], 0);
+    }
+
+    if (groupValue3 == 0 && keyPressed[col + 16]) {
+      keyPressed[col + 16] = false;
+      noteOn(0x91, keyToMidiMap[col + 16], 0);
+    }
+
+    if (groupValue4 == 0 && keyPressed[col + 24]) {
+      keyPressed[col + 24] = false;
+      noteOn(0x91, keyToMidiMap[col + 24], 0);
+    }
+    if (groupValue5 == 0 && keyPressed[col + 32]) {
+      keyPressed[col + 32] = false;
+      noteOn(0x91, keyToMidiMap[col + 32], 0);
+    }
+
+  }
+
+}
+
+
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
